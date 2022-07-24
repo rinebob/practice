46c877b1839394d0c57530adc420722b91202f16
rinebob <rinebob111185@gmail.com>
2022-07-24 07:38:15 -0700

    branch-2 commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,6 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     console.log('Hey this is the component!');
     console.log('I need another message here');
+    console.log('This message is in branch 2 dude...');
   }
 }
