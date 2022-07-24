b125e80c52650b97753add293064b097735f16bf
rinebob <rinebob111185@gmail.com>
2022-07-24 08:54:52 -0700

    branch-3 commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent implements OnInit {
     console.log('I need another message here because branch-1 needs a message!');
     console.log('This message is in branch 2 dude...');
     console.log('Welcome to branch 3!!');
+    console.log('Welcome to branch 3 commit 2');
 
 
   }
