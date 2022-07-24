231f1b7f6da11667d2273329933bad762d4f3159
rinebob <rinebob111185@gmail.com>
2022-07-24 07:26:00 -0700

    branch-1 commit 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'practice';
+
+  ngOnInit() {
+    console.log('Hey this is the component!')
+  }
 }
