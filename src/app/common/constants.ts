069923c7fd64b33af91968c268ccf8cc79f29fe5
rinebob <rinebob111185@gmail.com>
2022-07-30 11:51:19 -0700

    1 - Interfaces, constants and test data

---
+++ src/app/common/constants.ts
@@ -0,0 +1,5 @@
+
+
+export const DUDE = 'some_dude';
+export const TURKEY = 'some_turkey';
+export const UGLY_TURKEY = 'ugly_turkey';
---
+++ src/app/common/interfaces.ts
@@ -0,0 +1,13 @@
+
+
+export interface Dude {
+    id: string;
+    name: string;
+    age: string;
+}
+
+export interface Turkey {
+    id: string;
+    name: string;
+    ugly: boolean;
+}
