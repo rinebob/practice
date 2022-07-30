cb9b9297dbdccdd63f648205c7c6bcf7720d4ffe
rinebob <rinebob111185@gmail.com>
2022-07-30 11:45:49 -0700

    Delete test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,5 @@ export class AppComponent implements OnInit {
   title = 'practice';
 
   ngOnInit() {
-    console.log('Hey this is the component!');
-    console.log('I need another message here because branch-1 needs a message!');
-    console.log('This message is in branch 2 dude...');
-    console.log('Welcome to branch 3!!');
-    console.log('Welcome to branch 3 commit 2');
-    console.log('On to branch 4 commit 1');
-    console.log('On to branch 5 commit 1');
-    console.log('On to branch 5 commit 2');
-    console.log('On to branch 5 commit 4');
-    console.log('On to branch 5 commit 3');
-    console.log('On to branch 6 commit 1');
-    console.log('On to branch 6 commit 2');
-    console.log('On to branch 5 commit 4');
-    console.log('On to branch 6 commit 2 ammended');
-    console.log('On to branch 7 commit 1');
-
-
   }
 }
