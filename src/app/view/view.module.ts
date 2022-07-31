a3126d5eec63eeea48be8bb1646d6c1f5b2d14e4
rinebob <rinebob111185@gmail.com>
2022-07-31 09:03:31 -0700

    Implement panel component

--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewComponent } from './view.component';
+import { PanelComponent } from './panel/panel.component';
 
 
 
 @NgModule({
   declarations: [
     ViewComponent,
+    PanelComponent,
   ],
   imports: [
     CommonModule
